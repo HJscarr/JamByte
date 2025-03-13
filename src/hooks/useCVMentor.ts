ae460bda674b9cf02645c1ba2098fff6dd5f3778
HJscarr <jamesscarr6@gmail.com>
2025-03-13 17:36:18 +0000

    fix deployment

--- src/hooks/useCVMentor.ts
+++ src/hooks/useCVMentor.ts
@@ -54,7 +54,7 @@ export const useCV = (): UseCV => {
         },
         body: JSON.stringify({
           file_content: base64Data,
-          content_type: "application/pdf"
+          content_type: "text/plain"
         }),
       });
 
