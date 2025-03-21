d9ffa90b32b8f6d24c383b492f384562dd795b86
HJscarr <jamesscarr6@gmail.com>
2025-03-21 13:40:26 +0100

    fix: update descriptions

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,7 +1,7 @@
 export const courses = [
     {
         title: "Pi-Guard",
-        description: `A JamByte Course`,
+        description: `Learn programming, electronics, and networking by creating a home security device. It records audio and video when it detects motion!`,
         imageUrl: "/img/Pi-Guard/Pi-Guard-Card-Mobile.webp",
         author: "James Scarr",
         duration: "15 Videos",
@@ -12,14 +12,14 @@ export const courses = [
     },
     {
         title: "Rover",
-        description: `A JamByte Course`,
+        description: `Learn programming, electronics, and networking by creating an autonomous rover. It can explore its surroundings and avoid obstacles!`,
         status: "Coming soon",
         imageUrl: "/img/Rover/Rover.webp",
         details: `Create an autonomous rover that can explore its surroundings!`,
     },
     {
         title: "Link",
-        description: `A JamByte Course`,
+        description: `Learn programming, electronics, and networking by creating a device that can communicate with no WiFi or Mobile data!`,
         status: "Coming soon",
         imageUrl: "/img/Link/Link.webp",
         details: `Create device that can communicate with no WiFi or Mobile data!`,
